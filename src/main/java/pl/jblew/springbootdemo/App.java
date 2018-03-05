8beb13f50d45058dd8a551cf3ff8ce2082bfe321
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-03-05 19:56:13 +0100

    Basic docker, gradle and spring setup

---
+++ src/main/java/pl/jblew/springbootdemo/App.java
@@ -0,0 +1,19 @@
+package pl.jblew.springbootdemo;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class App {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+}
